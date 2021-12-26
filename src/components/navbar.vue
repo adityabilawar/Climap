d9b69b75cbb89879c09e488d70056a394e85939b
adityabilawar <23ab1107@wwprsd.org>
2021-12-26 13:04:14 -0500

    final navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,208 +1,37 @@
 <template>
-  <header :class=" { 'scrolled-nav': scrolledNav} ">
-    <nav>
-      <div class="branding">
-        <img src='src\assets\logo.png' alt="">
-      </div>
-      <ul v-show="!mobile" class="navigation">
-        <li><router-link class="link" :to="{name:'Home'}">Home</router-link></li>
-        <li><router-link class="link" :to="{name:'Home'}">About</router-link></li>
-        <li><router-link class="link" :to="{name:'Home'}">Download</router-link></li>
-        <li><router-link class="link" :to="{name:'Home'}">Contact</router-link></li>
-      </ul>
-      <div class='icon'>
-        <i @click="toggleMobileNav" v-show="mobile" class="far fa-bars" :class="{'icon-active' : mobileNav}"></i>
-      </div>
-      <transition name="mobile-nav">
-        <ul v-show="mobileNav" class="dropdown-nav">
-          <li><router-link class="link" :to="{name:'Home'}">Home</router-link></li>
-          <li><router-link class="link" :to="{name:'Home'}">About</router-link></li>
-          <li><router-link class="link" :to="{name:'Home'}">Download</router-link></li>
-          <li><router-link class="link" :to="{name:'Home'}">Contact</router-link></li>
-      </transition>
-    </nav>
-  </header>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+    <img src='C:\Users\adity\Documents\GitHub Repositories\climap\src\assets\logo.png' width="30" height="30" class="d-inline-block align-top" alt="">
+    Climap
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Download
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Dataset1</a>
+          <a class="dropdown-item" href="#">Dataset2</a>
+          <a class="dropdown-item" href="#">Dataset3</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
 </template>
 
-<script>
-export default {
-  name: "navbar",
-  data(){
-    return{
-      scrolledNav: null,
-      mobile: null,
-      mobileNav: null,
-      windowWidth: null,
-    };
-  },
-  created(){
-    window.addEventListener('resize', this.checkScreen);
-    this.checkScreen();
-  },
-  mounted(){
-    window.addEventListener('scroll', this.updateScroll);
-  },
-  methods:{
-    toggleMobileNav() {
-      this.mobileNav= !this.mobileNav;
-    },
 
-    updateScroll() {
-      const scrollPosition = window.scrollY;
-      if (scrollPosition > 50){
-        this.scrolledNav = true;
-        return;
-      }
-      this.scrolledNav = false;
-        return;
-    },
 
-    checkScreen(){
-      this.windowWidth = window.innerWidth;
-      if(this.windowWidth<=750){
-        this.mobile = true;
-        return;
-      }
-      this.mobile = false;
-      this.mobileNav = false;
-      return;
-    }
-  }
-};
-</script>
-
-<style lang="scss" scoped>
-
-header {
-  background-color: rgba(0, 0, 0, 0.8);
-  z-index: 99;
-  width: 100%;
-  position: fixed;
-  transition: .5s ease all;
-  color: #fff;
-
-  nav{
-    position: relative;
-    display: flex;
-    flex-direction: row;
-    padding: 12px 0;
-    transition: .5s ease all;
-    width: 90%;
-    margin: 0 auto;
-    @media (min-width: 1140px) {
-      max-width: 1140px;
-    }
-
-    ul,
-    .link {
-      font-weight: 500;
-      color: #fff;
-      list-style: none;
-      text-decoration: none;
-    }
-
-    li{
-      text-transform: uppercase;
-      padding: 16;
-      margin-left: 16px;
-    }
-
-    .link{
-      font-size: 14px;
-      transition: .5s ease all;
-      padding-bottom: 4px;
-      border-bottom: 1px solid transparent;
-
-      &:hover {
-        color: #00afea;
-        border-color: #00afea;
-      }
-    }
-
-    .branding{
-      display: flex;
-      align-items: center;
-
-      img{
-        width: 50px;
-        transition: .5s ease all;
-      }
-    }
-
-    .navigation{
-      display: flex;
-      align-items: center;
-      flex: 1;
-      justify-content: flex;
-    }
-    .icon {
-      display: flex;
-      align-items: center;
-      position: absolute;
-      top: 0;
-      right: 24px;
-      height: 100%;
-
-      i{
-        cursor: pointer;
-        font-size: 24px;
-        transition: .5s ease all;
-      }
-    }
-
-    .icon-active {
-      transform: rotate(180deg);
-    }
-
-    .dropdown-nav{
-      display: flex;
-      flex-direction: column;
-      position: fixed;
-      width: 100%;
-      max-width: 250px;
-      height: 100%;
-      background-color: #fff;
-      top: 0;
-      left: 0;
-
-      li{
-        margin-left: 0;
-        .link{
-          color:  #000;
-        }
-      }
-    }
-
-    .mobile-nav-enter-active,
-    .mobile-nav-leave-active{
-      transition: 1s ease all;
-    }
-
-    .mobile-nav-enter-from,
-    .mobile-nav-leave-to{
-      transform: translateX(-250px);
-    }
-
-    .mobile-nav-enter-to{
-       transform: translateX(0);
-    }
-  }
-}
-
-.scrolled-nav{
-  background-color: #000;
-  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
-
-  nav{
-    padding: 8px 0;
-    
-    .branding{
-      img{
-        width: 40px;
-        box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
-      }
-    }
-    
-  }
-}
-</style>
